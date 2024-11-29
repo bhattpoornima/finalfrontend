bee2ef356a4ba6570d6b1ff3466d451cec03bfb2
Poornima Bhatt <bhattpayal030@gmail.com>
2024-11-29 22:28:37 +0530

    debugged errors removed router form homepage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginPage from './pages/LoginPage';
 import SignInPage from './pages/SignInPage'; // Import SignInPage
 import EventForm from './components/EventForm';
 import MyEventsPage from './pages/MyEventsPage'; // Import MyEventsPage
+import EventDetails from './components/EventDetails';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/events/add" element={<EventForm isEdit={false} />} />
         <Route path="/events/:id/edit" element={<EventForm isEdit={true} />} />
         <Route path="/my-events" element={<MyEventsPage />} />
+        <Route path="/event/:eventId" element={<EventDetails />} />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,18 @@
 // src/pages/HomePage.js
 import './homepage.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EventList from '../components/EventList';
 import EventDetails from '../components/EventDetails';
 
-
 const HomePage = () => {
   return (
-    <Router>
-      <div className="homepage-container">
-        {/* Big welcome text */}
-        <h1 className="welcome-text">Welcome to Event Master!</h1>
-        
-        {/* Routes for EventList and EventDetails */}
-        <Routes>
-          <Route path="/" element={<EventList />} />
-          <Route path="/event/:eventId" element={<EventDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Welcome to the Event Master</h1>
+      <EventList />
+      {/* Other content */}
+    </div>
   );
 };
 
 export default HomePage;
+
